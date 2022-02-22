2287cb041d586c35b768069a84309aa3f58caf9c
Diego Rojas <diegorojas431@gmail.com>
2022-02-22 18:00:36 -0500

    Mostrando cada pregunta en el Browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { Challenges } from './components/Challenges';
 import { Intro } from './components/Intro';
 
 export const App = () => {
 
   const [isBeginning, setIsBeginning] = useState(false)
+  const [results, setResults] = useState({})
+
 
   const handleClick = () => setIsBeginning(true)
 
@@ -11,7 +14,7 @@ export const App = () => {
     <>
       {
         isBeginning
-          ? <h1>Comenzar Pruebas</h1>
+          ? <Challenges setResults={setResults}/>
           : <Intro handleClick={handleClick}/>
       }
     </>
--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -2,13 +2,47 @@ import React, { useEffect, useState } from 'react';
 import { getData } from '@utils/getData';
 
 export const Challenges = () => {
-  useEffect(() => {
 
+  const [ data, setData] = useState({
+    loading: true,
+    data: []
+  });
+
+  const [level, setLevel] = useState(0)
+
+  useEffect(() => {
     getData()
-      .then(console.log)
-  }, [])
+      .then( data => setData({
+        loading: false,
+        questions: data
+      }));
+
+  }, []);
+
+  const handleClick = (result) => {
+    setLevel(level + 1)
+  }
+
+  const { loading, questions } = data;
+
 
   return (
-    <div>Challenges</div>
+    <div className='question'>
+
+      {
+        !loading && (
+          <>
+            <h1>{questions[level].category}</h1>
+            <h2>{questions[level].question}</h2>
+            <h3>{`Level ${level + 1}/10`}</h3>
+            <div>
+              <button onClick={handleClick}>True</button>
+              <button onClick={handleClick}>False</button>
+            </div>
+          </>
+        )
+      }
+
+    </div>
   )
 }
