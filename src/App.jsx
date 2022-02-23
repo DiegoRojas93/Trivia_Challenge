a296ccb8ac8176491f0ea7c0cdcb808c02aa1bc8
Diego Rojas <diegorojas431@gmail.com>
2022-02-22 20:56:17 -0500

    Retornando al home otra vez para inicializar el Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Challenges } from './components/Challenges';
 import { Intro } from './components/Intro';
+
+import { getData } from '@utils/getData';
 
 export const App = () => {
 
   const [isBeginning, setIsBeginning] = useState(false)
-  const [results, setResults] = useState({})
+  const [results, setResults] = useState([])
 
+  const [ data, setData] = useState({
+    loading: true,
+    questions: []
+  });
 
-  const handleClick = () => setIsBeginning(true)
+  useEffect(() => {
+    getData()
+      .then( data => setData({
+        loading: false,
+        questions: data
+      }));
+  }, []);
+
+  const handleStart = () => setIsBeginning(true)
+  const handleAgain = () => setIsBeginning()
 
   return (
     <>
       {
         isBeginning
-          ? <Challenges setResults={setResults}/>
-          : <Intro handleClick={handleClick}/>
+          ? <Challenges data={data} setResults={setResults} results={results} handleAgain={handleAgain}/>
+          : <Intro handleStart={handleStart}/>
       }
     </>
   )
--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,46 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { getData } from '@utils/getData';
+import React, { useState } from 'react';
 
-export const Challenges = () => {
 
-  const [ data, setData] = useState({
-    loading: true,
-    data: []
-  });
+export const Challenges = ({ data, setResults, results, handleAgain }) => {
 
   const [level, setLevel] = useState(0)
 
-  useEffect(() => {
-    getData()
-      .then( data => setData({
-        loading: false,
-        questions: data
-      }));
-
-  }, []);
-
   const handleClick = (result) => {
+    setResults( r => [ ...r, { ...questions[ level ], respUser: result } ] )
     setLevel(level + 1)
   }
 
   const { loading, questions } = data;
 
-
   return (
     <div className='question'>
 
       {
-        !loading && (
-          <>
-            <h1>{questions[level].category}</h1>
-            <h2>{questions[level].question}</h2>
-            <h3>{`Level ${level + 1}/10`}</h3>
-            <div>
-              <button onClick={handleClick}>True</button>
-              <button onClick={handleClick}>False</button>
-            </div>
-          </>
-        )
+        !loading && level <= 9
+          ? (
+              <>
+                <h1>{questions[level].category}</h1>
+                <h2>{questions[level].question}</h2>
+                <h3>{`Level ${level + 1}/10`}</h3>
+                <div>
+                  <button onClick={() => handleClick(true)}>True</button>
+                  <button onClick={() => handleClick(false)}>False</button>
+                </div>
+              </>
+            )
+          : (
+              <>
+                <h1>You Score</h1>
+                <h2>{`${'ggg'}/10`}</h2>
+                
+                <button onClick={ handleAgain }>Play Again</button>
+              </>
+          )
       }
 
     </div>
