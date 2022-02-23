a8cc52c3ee0069f6b6f5a68f9a1fff03da21ad18
Diego Rojas <diegorojas431@gmail.com>
2022-02-23 17:47:57 -0500

    Resetiendo el CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+import '@styles/reset.scss';
 import '@styles/style.scss';
 
 const $root = document.querySelector('#root');
